51b7aa69de4c15fc1371be432914a0f1c698bf55
Bonfim Justino do Nascimento <bonfim1999@gmail.com>
2020-06-06 18:33:27 -0300

    ReadOnly Admin Consultas

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -25,6 +25,23 @@ class AgendaAdmin(admin.ModelAdmin):
 class HorarioAdmin(admin.ModelAdmin):
     pass
 
+
+class ReadOnlyAdmin(admin.ModelAdmin):
+    readonly_fields = []
+
+    def get_readonly_fields(self, request, obj=None):
+        return list(self.readonly_fields) + \
+               [field.name for field in obj._meta.fields] + \
+               [field.name for field in obj._meta.many_to_many]
+
+    def has_add_permission(self, request):
+        return False
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        s = super(ReadOnlyAdmin, self).change_view(request, object_id, form_url, extra_context)
+        s.context_data['change'] = False
+        return s
+
 @admin.register(Consulta)
-class ConsultaAdmin(admin.ModelAdmin):
+class ConsultaAdmin(ReadOnlyAdmin):
     list_display = ('agenda', 'data_agendamento', 'user')
--- backend/core/form.py
+++ backend/core/form.py
@@ -13,7 +13,7 @@ class AgendaForm(forms.ModelForm):
         agendas = Agenda.objects.filter(dia=dados['dia'], medico=dados['medico'])
 
         if agendas and not self.instance.id:
-            return self.add_error('dia', self.error_class(['Este médico já agendado para esse dia.']))
+            return self.add_error('dia', self.error_class(['Este médico já está agendado para esse dia.']))
 
         if dados['dia'] < datetime.datetime.now().date():
             return self.add_error('dia', self.error_class(['Não é possivel marcar agendas para dias passados.']))
